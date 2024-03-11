fdea8b75d2bf3a617a7f7058faa3dd62f8e620cd
L-LIFE <seodbsk78@gmail.com>
2024-03-12 07:13:47 +0900

    Python1

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-# This is a sample Python script.
+# 정수 계산
+10+5
+(10-5)*2
+10/2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#실수 계산
+2.2*5
+1.0/0.2
+3/2
 
+# /, //, %
+3/2
+3//2
+5/2
+5//2
+5%2
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print()c
+#거듭제곱
+2**5
+2**10
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#변수 사용
+a=10 # 변수명은 숫자로 시작과 특수 기호, 시스템 키워드도 사용할 수 없다.
+b=20
+a*b
